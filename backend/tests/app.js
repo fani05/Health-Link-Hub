17a119abae7384a232cda6d7fc3050f2f78820ea
fani <fabian.nica09@gmail.com>
2026-06-06 20:42:15 +0300

    Implemented tests for Medical Records

--- backend/tests/app.js
+++ backend/tests/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRoutes from '../src/routes/auth_routes.js';
 import appointmentRoutes from '../src/routes/appointment_routes.js';
+import medicalRecordRoutes from '../src/routes/medical_record_routes.js';
 
 const app = express();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/appointments', appointmentRoutes);
+app.use('/api/medical-records', medicalRecordRoutes);
 
 export default app;
